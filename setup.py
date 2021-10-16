891af0d8a3b2d5712a76a61c91ce65479bfc045f
gezp <zhenpeng.ge@qq.com>
2021-10-16 20:47:07 +0800

    new release

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-
 from os import path
+
+from setuptools import find_packages, setup
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='xmacro',  
-    version='1.1.1',  
-    author='Zhenpeng Ge',  
-    author_email='zhenpeng.ge@qq.com', 
-    url='https://github.com/gezp/xmacro', 
-    description='a simple XML macro script',  
+    name='xmacro',
+    version='1.1.2',
+    author='Zhenpeng Ge',
+    author_email='zhenpeng.ge@qq.com',
+    url='https://github.com/gezp/xmacro',
+    description='a simple XML macro script',
     long_description=long_description,
     long_description_content_type='text/markdown',
     license='MIT',
-    keywords=['xml','macro','xmacro','sdformat','urdf'],
+    keywords=['xml', 'macro', 'xmacro', 'sdformat', 'urdf'],
     include_package_data=True,
     packages=find_packages(),
-    entry_points={ 
+    entry_points={
         'console_scripts': [
             'xmacro = xmacro:xmacro_main',
             'xmacro4sdf = xmacro:xmacro4sdf_main',
